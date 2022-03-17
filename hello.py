ba8279840a3e4a4cdbd438538690816cec0b4d3f
Afiq Hafizuddin <hafizuddinafiq@gmail.com>
2022-03-17 17:48:23 +0800

    add check_name function

--- hello.py
+++ hello.py
@@ -1,4 +1,17 @@
+from operator import le
+
+
 name = 'Afiq Hafizuddin bin Zainal'
 
 print(name)
 print('Hello, world')
+
+def check_name(name):
+    if len(name) >= 15:
+        print('Invalid username')
+    elif len(name) <= 3:
+        print('Invalid username')
+    else:
+        print("Valid username")
+
+check_name(name)
